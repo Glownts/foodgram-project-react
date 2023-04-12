dda60cc0f72174722f71b935e43b6eccaffeb9b1
Konstantin <kostya_ostapenko_2014@mail.ru>
2023-04-12 16:53:20 +0300

    Add script for start project

--- infra/script.sh
+++ infra/script.sh
@@ -1,7 +1,6 @@
-docker-compose exec backend python manage.py createsuperuser
-    Email: "admin@mail.ru"
-    Username: "admin"
-    First_name: "admin"
-    Last_name: "admin"
-    Password: "admin"
-    Password (again): "admin"
+docker-compose up -d --build
+docker-compose exec backend python manage.py makemigrations
+docker-compose exec backend python manage.py migrate
+docker-compose exec backend python manage.py collectstatic --no-input
+docker-compose exec backend python manage.py load_data
+docker-compose exec backend python manage.py createsuperuser --email admin@mail.ru --username admin --first_name admin --last_name admin
